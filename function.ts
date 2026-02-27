0b3e980b60cbd2b9489394e997ff1c68e47980b3
Yasmin Gusmão <yagusmao@gmail.com>
2026-02-26 21:33:44 -0300

    Subindo arquivo TS

---
+++ function.ts
@@ -0,0 +1,12 @@
+function multiplicaDoisNumeros(a: number, b: number): number {
+    return a * b;
+}
+
+console.log(multiplicaDoisNumeros(4, 2));
+
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}!`;
+}
+
+console.log(saudacao("Yasmin"));
